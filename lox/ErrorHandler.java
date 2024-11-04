5ff7d003351ae0520f73a044bf789b3e1c16e4b0
Mih1rGupt4 <mihirgupta422@gmail.com>
2024-11-04 22:44:17 +0530

    Extracted error handling functions into jloxinterpreter.lox package

---
+++ lox/ErrorHandler.java
@@ -0,0 +1,16 @@
+package jloxinterpreter.lox;
+
+public class ErrorHandler {
+    public static boolean hadError = false;
+
+    public static void error(int line, String message) {
+        report(line, "", message);
+      }
+    
+      private static void report(int line, String where,
+          String message) {
+        System.err.println(
+            "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+      }
+}
